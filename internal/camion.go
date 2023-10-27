92b647cf9e78b76b327f8b671ee1b04e428913e2
DarckMonster <juanfcv2001@hotmail.com>
2023-10-27 13:33:28 +0200

    archivo creado planificacion.go y modificación de camion.go advances #14

--- internal/camion.go
+++ internal/camion.go
@@ -9,7 +9,12 @@ const (
 )
 
 type Camion struct {
-	tipo      tipoCamion
-	capacidad [3]float32
-	mma       uint
+	tipo      		tipoCamion
+	capacidad 		[3]float32
+	mma       		uint
+	suministros		[]Suministro
+}
+
+func (c Camion) getSuministro(i uint) Suministro {
+	return c.suministros[i]
 }
