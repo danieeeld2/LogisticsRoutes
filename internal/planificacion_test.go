a0fef0bc16bb29860caf5a18bd8cb7456ca8122e
danieeeld2 <danieeeld2@gmail.com>
2023-11-09 18:48:32 +0100

    Implementada funcion que ve si un camion es mas pequeño que otro
    Closes #34
    
    Avanza #32

--- internal/planificacion_test.go
+++ internal/planificacion_test.go
@@ -11,15 +11,46 @@ func PuedeTransportarSuministro(camiones []Camion, suministro Suministros) bool 
 		}
 	}
 	
-	uint suma_mma = 0
-	float32 volumen_total = 0
-	float32 volumen_suministro = suministro.dimensiones_whd_cm[0]*suministro.dimensiones_whd_cm[1]*suministro.dimensiones_whd_cm[2]
+	var suma_mma uint
+	var volumen_total, volumen_suministro float32
+	
+	suma_mma = 0 
+	volumen_total = 0 
+	volumen_suministro = suministro.dimensiones_whd_cm[0]*suministro.dimensiones_whd_cm[1]*suministro.dimensiones_whd_cm[2] 
 	for _, camion := range camiones {
 		suma_mma += camion.mma
 		volumen_total += camion.dimensiones_whd_cm[0]*camion.dimensiones_whd_cm[1]*camion.dimensiones_whd_cm[2]
 	}
 	
 	if suma_mma < suministro.mma || volumen_total < volumen_suministro {
+		return false
+	}
+	
+	return true
+}
+
+func EsMasChico(camion1 []Camion, camion2 []Camion) bool {
+	if len(camion1) > len(camion2) {
+		return false
+	}
+	
+	var suma_mma_1, suma_mma_2 uint
+	var volumen_total_1, volumen_total_2 float32
+	
+	suma_mma_1 = 0
+	suma_mma_2 = 0
+	volumen_total_1 = 0
+	volumen_total_2 = 0
+	for _, camion := range camion1 {
+		suma_mma_1 += camion.mma
+		volumen_total_1 += camion.dimensiones_whd_cm[0] * camion.dimensiones_whd_cm[1] * camion.dimensiones_whd_cm[2]
+	}
+	for _, camion := range camion2 {
+		suma_mma_2 += camion.mma
+		volumen_total_2 += camion.dimensiones_whd_cm[0] * camion.dimensiones_whd_cm[1] * camion.dimensiones_whd_cm[2]
+	}
+	
+	if suma_mma_2 < suma_mma_1 && volumen_total_2 < volumen_total_1 {
 		return false
 	}
 	
