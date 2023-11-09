d71d70002dbce088fc19dfaa1213ff19f5ce4b22
danieeeld2 <danieeeld2@gmail.com>
2023-11-09 15:32:43 +0100

    Añado funciones que sirvan como constructores para poder plantear los tests
    Avanza #29

--- internal/camion.go
+++ internal/camion.go
@@ -5,3 +5,11 @@ type Camion struct {
 	dimensiones_whd_cm			[3]float32
 	mma       				uint
 }
+
+func NewCamion(tipo TipoSuministro, dimensiones [3]float32, mma uint) Camion {
+	return Camion{
+		tipo:			tipo,
+		dimensiones_whd_cm:	dimensiones,
+		mma:			mma,
+	}
+}
--- internal/suministro.go
+++ internal/suministro.go
@@ -9,3 +9,13 @@ type Suministro struct {
 	dimensiones_whd_cm			[3]float32
 	tipo					TipoSuministro
 }
+
+func NuevoSuministro(direccion string, tiempo time.Duration, peso_kg float32, dimensiones [3]float32, tipo TipoSuministro) Suministro {
+	return Suministro{
+		direccion:           direccion,
+		tiempo:              tiempo,
+		peso_kg:             peso_kg,
+		dimensiones_whd_cm: dimensiones,
+		tipo:                tipo,
+	}
+}
