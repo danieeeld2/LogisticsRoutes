4cc8a9390c4a3a92472ad0d709ac1c6ed2b9b7e1
danieeeld2 <danieeeld2@gmail.com>
2023-11-10 12:30:37 +0100

    Fixeo toma de conjuntos
    Closes #37

--- internal/planificacion.go
+++ internal/planificacion.go
@@ -8,4 +8,33 @@ type Planificacion struct {
 
 
 func AsigarCamiones(CamionesDisponibles *[]Camion, suministro Suministro, CamionesAsignados *[]Camion) {
+	
+}// ALGORITMO DE ASIGNACION
+// FILEPATH: /home/daniel/Git/LogisticsRoutes/internal/planificacion.go
+
+func AsignarCamiones(CamionesDisponibles *[]Camion, suministro Suministro, CamionesAsignados *[]Camion) {
+	// Si no hay camiones disponibles, terminamos
+	if len(*CamionesDisponibles) == 0 {
+		return
+	}
+
+	// Buscamos si hay algún camión individual que pueda cargar el suministro
+	camionIndividual := BuscarCamionIndividual(CamionesDisponibles, suministro)
+	if camionIndividual != nil {
+		*CamionesAsignados = append(*CamionesAsignados, *camionIndividual)
+		eliminarCamion(CamionesDisponibles, camionIndividual)
+		return
+	}
+
+	// Si no hay camiones individuales, buscamos en los subconjuntos de camiones disponibles
+	for i := 1; i <= len(*CamionesDisponibles); i++ {
+		subconjuntos := Combinaciones(*CamionesDisponibles, i)
+		for _, subconjunto := range subconjuntos {
+			if PuedeCargar(subconjunto, suministro) {
+				*CamionesAsignados = append(*CamionesAsignados, subconjunto...)
+				eliminarCamiones(CamionesDisponibles, subconjunto)
+				return
+			}
+		}
+	}
 }
