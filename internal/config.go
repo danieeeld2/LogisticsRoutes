a7d061c25631a35c4f95adc08def32df7a4a1da3
danieeeld2 <danieeeld2@gmail.com>
2023-12-06 03:39:12 +0100

    :construction:Prueba para ver si lee variables avanza #88

--- internal/config.go
+++ internal/config.go
@@ -4,7 +4,10 @@ import (
 	"github.com/knadh/koanf"
 	"github.com/knadh/koanf/providers/file"
 	"github.com/knadh/koanf/parsers/yaml"
-	"github.com/knadh/koanf/providers/env"
+	// "github.com/knadh/koanf/providers/env"
+	"github.com/joho/godotenv"
+	"os"
+	"fmt"
 )
 
 type AppConfig struct {
@@ -31,11 +34,20 @@ type AppConfig struct {
 func loadConfig(confiFile string) (*AppConfig, error) {
 	k := koanf.New(".")
 	if err := k.Load(file.Provider(confiFile), yaml.Parser()); err != nil {
-		if err := k.Load(env.Provider("LOGISTICSROUTES_", ".", func(s string) string {
-			// Puedes personalizar la transformación de nombres de variables aquí si es necesario
-			return s
-		}), nil); err != nil {
+		// if err := k.Load(env.Provider("LOGISTICSROUTES_", ".", func(s string) string {
+		// 	// Puedes personalizar la transformación de nombres de variables aquí si es necesario
+		// 	return s
+		// }), nil); err != nil {
+		// 	return nil, err
+		// }
+		if err := godotenv.Load(); err != nil {
 			return nil, err
+		}
+		envVars := os.Environ()
+
+		// Imprimir todas las variables de entorno
+		for _, envVar := range envVars {
+			fmt.Println(envVar)
 		}
 	}
 
