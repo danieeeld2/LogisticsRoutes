b3a6928dd23a7a417a1ace3308eba652a429d969
danieeeld2 <danieeeld2@gmail.com>
2023-12-22 14:04:59 +0100

    :bug: Uso las buenas practicas en la creacion de la API closes #106
    Buen uso del singular y el plural y la identificacion de recursos

--- internal/api.go
+++ internal/api.go
@@ -10,42 +10,47 @@ var router *chi.Mux
 func createRouter() *chi.Mux{
 	if router == nil {
 		router = chi.NewRouter()
-		router.Get("/config", getConfig)
-		router.Get("/camiones", getCamiones)
-		router.Post("/camiones", createCamion)
-		router.Get("/suministros", getSuministros)
-		router.Post("/suministros", createSuministro)
-		router.Post("/planificacion", planificarEntrega)
-		router.Get("/planificacion", getEntrega)
+		router.Get("/camion/{matricula}", getCamiom)
+		router.Put("/camion/{matricula}", putCamion)
+		router.Delete("/camion/{matricula}", deleteCamion)
+		router.Get("/camion/{matricula}/asignacion", getAsignacion)
+		router.Put("/camion/{matricula}/asignacion", putAsignacion)
+		router.Get("/suministro/{id}", getSuministro)
+		router.Put("/suministro/{id}", putSuministro)
+		router.Delete("/suministro/{id}", deleteSuministro)
 	}
 
 	return router
 }
 
-func getConfig(w http.ResponseWriter, r *http.Request){
+func getCamiom(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func getCamiones(w http.ResponseWriter, r *http.Request){
-
-}
-
-func createCamion(w http.ResponseWriter, r *http.Request){
+func putCamion(w http.ResponseWriter, r *http.Request) {
 	
 }
 
-func getSuministros(w http.ResponseWriter, r *http.Request){
+func deleteCamion(w http.ResponseWriter, r *http.Request) {
 	
 }
 
-func createSuministro(w http.ResponseWriter, r *http.Request){
+func getAsignacion(w http.ResponseWriter, r *http.Request) {
 	
 }
 
-func planificarEntrega(w http.ResponseWriter, r *http.Request){
+func putAsignacion(w http.ResponseWriter, r *http.Request) {
 	
 }
 
-func getEntrega(w http.ResponseWriter, r *http.Request){
+func getSuministro(w http.ResponseWriter, r *http.Request) {
 	
 }
+
+func putSuministro(w http.ResponseWriter, r *http.Request) {
+	
+}
+
+func deleteSuministro(w http.ResponseWriter, r *http.Request) {	
+	
+}	
