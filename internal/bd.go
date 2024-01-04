0119775907bb5ecb5b3d5fd6899c8d633d242961
danieeeld2 <danieeeld2@gmail.com>
2024-01-04 14:10:53 +0100

    Comienzo creacion de interfaces y simular BD, Avanza #107
    Creo structs para tener camiones con matrículas y suministros con IDs. Falat ver como solventar la asignación

---
+++ internal/bd.go
@@ -0,0 +1,79 @@
+package internal
+
+type CamionMatricula struct {
+	camion Camion
+	matricula string
+}
+
+type SuministroID struct {
+	suministro Suministro
+	id string
+}
+
+type BD struct {
+	camiones []CamionMatricula
+	suministros []SuministroID
+}
+
+func getCamionMatricula(matricula string, bd BD) (CamionMatricula, bool) {
+	for _, CamionMatricula := range bd.camiones {
+		if CamionMatricula.matricula == matricula {
+			return CamionMatricula, true
+		}
+	}
+	return CamionMatricula{}, false
+}
+
+func getSuministroID(id string, bd BD) (SuministroID, bool) {
+	for _, SuministroID := range bd.suministros {
+		if SuministroID.id == id {
+			return SuministroID, true
+		}
+	}
+	return SuministroID{}, false
+}
+
+func deleteCamionMatricula(matricula string, bd BD) (CamionMatricula, bool) {
+	for i, CamionMatricula := range bd.camiones {
+		if CamionMatricula.matricula == matricula {
+			bd.camiones = append(bd.camiones[:i], bd.camiones[i+1:]...)
+			return CamionMatricula, true
+		}
+	}
+	return CamionMatricula{}, false
+}
+
+func deleteSuministroID(id string, bd BD) (SuministroID, bool) {
+	for i, SuministroID := range bd.suministros {
+		if SuministroID.id == id {
+			bd.suministros = append(bd.suministros[:i], bd.suministros[i+1:]...)
+			return SuministroID, true
+		}
+	}
+	return SuministroID{}, false
+}
+
+func putCamionMatricula(NuevoCamionMatricula CamionMatricula, bd BD) (CamionMatricula, bool) {
+	for i, CamionMatricula := range bd.camiones {
+		if CamionMatricula.matricula == NuevoCamionMatricula.matricula {
+			bd.camiones[i] = NuevoCamionMatricula
+			return NuevoCamionMatricula, true
+		}
+	}
+	return CamionMatricula{}, false
+}
+
+func putSuministroID(NuevoSuministroID SuministroID, bd BD) (SuministroID, bool) {
+	for i, SuministroID := range bd.suministros {
+		if SuministroID.id == NuevoSuministroID.id {
+			bd.suministros[i] = NuevoSuministroID
+			return NuevoSuministroID, true
+		}
+	}
+	return SuministroID{}, false
+}
+
+
+
+
+
